312ad192fc5a369196e06ecf779b887dbed0346f
rysavfi1 <rysavyfilip.developer@gmail.com>
2022-08-04 22:55:17 +0200

    Add template of Spring Boot project using Gradle

---
+++ template-api/src/test/java/cz/frysavy/template/TemplateApiApplicationTests.java
@@ -0,0 +1,15 @@
+package cz.frysavy.template;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
